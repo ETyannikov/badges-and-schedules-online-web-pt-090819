191625295b8b3c798f70d9b7d928c97a42b0bebf
Eddy Tyannikov <etyannikov@gmail.com>
2019-09-17 22:34:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,6 @@ end
 
 def batch_badge_creator(array)
   array2 = []
-  array2 = array
   array.each_with_index do |name, index|
     array2[index] = badge_maker(name)
     puts array2[index]
@@ -15,8 +14,6 @@ end
 def assign_rooms(array)
   array2 = []
   array.each_with_index do |name, index|
-  array2 = array
-  array2.each_with_index do |name, index|
     array2[index] ="Hello, #{name}! You'll be assigned to room #{index + 1}!"
     puts array2[index]
   end
@@ -26,14 +23,6 @@ end
 def printer(array)
    batch_badge_creator(array)
    assign_rooms(array)
-   array1 =batch_badge_creator(array)
-   array2 =assign_rooms(array)
-   array1.each_with_index do |name,index|
-     puts array1[index]
-   end
-    array2.each_with_index do |name,index|
-     puts array2[index]
-   end
 end
 
 
